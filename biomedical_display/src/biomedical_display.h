86de59a4f1885a353814fe652c66846a275a64d3
Antoine Villeret <antoine.villeret@gmail.com>
2019-01-30 20:30:30 +0100

    WIP on raspberry pi support

--- biomedical_display/src/biomedical_display.h
+++ biomedical_display/src/biomedical_display.h
@@ -15,8 +15,12 @@ class biomedical_display : public ofBaseApp
 
     void messageReceived (ofMessage&);
     void windowResized   (ofResizeEventArgs&);
-
-    ofxShadertoy& get_shader() { return m_shader; }
+#ifdef TARGET_RASPBERRY_PI
+    ofShader&
+#else
+    ofxShadertoy&
+#endif
+      get_shader() { return m_shader; }
 
     std::map<std::string, std::pair<std::string, float>> m_uniform_map;
 
@@ -24,8 +28,14 @@ class biomedical_display : public ofBaseApp
     std::vector<Oscilloscope> m_oscillos;
     opp::oscquery_server m_server;
 
-    ofxShadertoy m_shader;
-    ofFbo m_fbo;
+#ifdef TARGET_RASPBERRY_PI
+    ofShader
+#else
+    ofxShadertoy
+#endif
+      m_shader;
+
+   ofFbo m_fbo;
    ofFbo m_PALfbo;
 
    ofVec2f render_size;
--- biomedical_display/src/main.cpp
+++ biomedical_display/src/main.cpp
@@ -2,9 +2,14 @@
 
 int main()
 {
+#ifdef TARGET_RASPBERRY_PI
+  ofGLESWindowSettings settings;
+  settings.glesVersion = 2;
+#else
   ofGLWindowSettings settings;
   settings.setGLVersion(3,2);
   settings.setSize(1024,768);
+#endif
 
   auto window = ofCreateWindow(settings);
   auto app = make_shared<biomedical_display>();
