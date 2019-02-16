5508ef81b8384aa17f63c82b5f0919de626a609e
Antoine Villeret <antoine.villeret@gmail.com>
2019-02-16 11:58:25 +0100

    fix start.sh after hostname change

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export PATH=/opt/gcc-8.2.0/bin:$PATH
 export LD_LIBRARY_PATH=/opt/gcc-8.2.0/lib:$LD_LIBRARY_PATH
 HERE=`dirname $(readlink -f $0)`
 
-if [ "${HOSTNAME}" == "ECG" ]; then
+if [ "${HOSTNAME}" == "ecg" ]; then
 	${HERE}/biomedical_display-armv7-Release
 else
 	${HERE}/video_player-armv7-Release
