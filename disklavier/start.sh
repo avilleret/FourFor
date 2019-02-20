41389c0e0662166e94512b9bf76c85805748397f
Antoine Villeret <antoine.villeret@gmail.com>
2019-02-20 11:16:27 +0100

    update disklavier startup script

--- disklavier/start.sh
+++ disklavier/start.sh
@@ -5,6 +5,7 @@ HERE=`dirname $(readlink -f $0)`
 killall pd
 
 /usr/local/bin/pd -noaudio -alsamidi -mididev 1 -nogui ${HERE}/midi_over_udp.pd &
+PDPID=$!
 sleep 0.5
 
 aconnect -x
@@ -14,5 +15,7 @@ sudo aconnect 20:0 128:0
 sudo aconnect 20:1 128:0
 sudo aconnect 128:1 20:0
 sudo aconnect 128:1 20:1
+
+wait $PDPID
 
 sleep 0.5
